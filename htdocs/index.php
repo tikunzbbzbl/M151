5c5704a63cd6e1efc752732352591e7127486be0
tikunzbbzbl <157618426+tikunzbbzbl@users.noreply.github.com>
2025-01-23 11:21:19 +0100

    init

---
+++ htdocs/index.php
@@ -0,0 +1,30 @@
+<?php
+// Vorname und Name als String gespeichert werden, muss man sie in Anführungszeichen schreiben.
+// Der Jahrgang wird ohne Anführungszeichen gespeichert, da es eine Zahl ist
+
+$vorname = "Tim"; // String
+$name = "Kunz"; // String
+$jahrgang = 1998; // Zahl 
+
+// Ausgabe der Variablen
+echo "Vorname: $vorname\n";
+echo "Name: $name\n";
+echo "Jahrgang: $jahrgang\n";
+?>
+
+
+<!DOCTYPE html>
+<html lang="<?php echo $language; ?>">
+  <head>
+    <meta charset="UTF-8">
+    <title>Auträge M151</title>
+  </head>
+  <body>
+    echo "Übung macht den Meister" <br>
+    echo "es ist noch kein Meister vom Himmel gefallen"
+
+
+
+    
+  </body>
+</html>
