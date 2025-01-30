addbd6ab719e33c3bc778b06af2ecffb6f360995
tikunzbbzbl <157618426+tikunzbbzbl@users.noreply.github.com>
2025-01-30 08:06:58 +0100

    push

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,24 @@
+<!DOCTYPE html>
+<html lang="<?php echo $language; ?>">
+  <head>
+    <meta charset="UTF-8">
+    <title>Auträge M151</title>
+  </head>
+  <body>
+    "Übung macht den Meister" <br>
+    "es ist noch kein Meister vom Himmel gefallen" <br>
+  </body>
+</html>
 <?php
 // Vorname und Name als String gespeichert werden, muss man sie in Anführungszeichen schreiben.
 // Der Jahrgang wird ohne Anführungszeichen gespeichert, da es eine Zahl ist
+
+//How-To String
+// 1. Mit dem Punkt (.) Operator
+// 2. Mit dem Zuweisungsoperator (.=), um einen bestehenden String zu erweitern
+
+$text = "Hallo";
+$text .= " Welt!"; // Ergebnis: "Hallo Welt!"
 
 $vorname = "Tim"; // String
 $name = "Kunz"; // String
@@ -9,22 +27,7 @@ $jahrgang = 1998; // Zahl
 // Ausgabe der Variablen
 echo "Vorname: $vorname\n";
 echo "Name: $name\n";
-echo "Jahrgang: $jahrgang\n";
+echo "Jahrgang: $jahrgang\n <br>";
+echo "Mein Name ist $vorname $name und ich bin $jahrgang geboren.<br>";
+echo $text;
 ?>
-
-
-<!DOCTYPE html>
-<html lang="<?php echo $language; ?>">
-  <head>
-    <meta charset="UTF-8">
-    <title>Auträge M151</title>
-  </head>
-  <body>
-    echo "Übung macht den Meister" <br>
-    echo "es ist noch kein Meister vom Himmel gefallen"
-
-
-
-    
-  </body>
-</html>
