333d39308dedfb9af91954149db41e1571e4cd0f
tikunzbbzbl <157618426+tikunzbbzbl@users.noreply.github.com>
2025-01-30 11:22:10 +0100

    lotto

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,7 @@
 
 // Initialisierung
 $error = '';
+$message = '';
 $firstname = $lastname = $email = $username = '';
 
 // Wurden Daten mit "POST" gesendet?
@@ -11,6 +12,21 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
   print_r($_POST);
   echo "</pre>";
 
+
+if (empty($_POST["firstname"]) || empty($_POST["lastname"]) || empty($_POST["username"])){
+    $error = "Vor- und Nachname und Benutzername erforderlich!";
+} elseif (strlen($_POST["firstname"]) > 30 || strlen($_POST["lastname"]) > 30 || strlen($_POST["username"]) > 30) {
+    $error = "Maximal 30 Zeichen erlaubt in Vor- und Nachname und Benutzername!";
+} else {
+    $firstname = htmlspecialchars($_POST["firstname"]);
+    $lastname = htmlspecialchars($_POST["lastname"]);
+    $username = htmlspecialchars($_POST["username"]);
+}
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+  $error .= "Ungültige E-Mail-Adresse!<br>";
+} else {
+  $email = htmlspecialchars($_POST["email"]);
+}
   /** TODO 
    * alle Benutzereingaben gemäss Auftrag validieren
    * Variable vorhanden
@@ -63,36 +79,36 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         <!-- TODO: Clientseitige Validierung: vorname -->
         <div class="form-group">
           <label for="firstname">Vorname *</label>
-          <input type="text" name="firstname" class="form-control" id="firstname"
+          <input type="text" name="firstname" maxlength="35" class="form-control" id="firstname"
                   value="<?php echo htmlspecialchars($firstname) ?>"
-                  placeholder="Geben Sie Ihren Vornamen an.">
+                  required placeholder="Geben Sie Ihren Vornamen an.">
         </div>
         <!-- TODO: Clientseitige Validierung: nachname -->
         <div class="form-group">
           <label for="lastname">Nachname *</label>
-          <input type="text" name="lastname" class="form-control" id="lastname"
+          <input type="text" name="lastname" maxlength="35" class="form-control" id="lastname"
                   value="<?php echo htmlspecialchars($lastname) ?>"
-                  placeholder="Geben Sie Ihren Nachnamen an">
+                  required placeholder="Geben Sie Ihren Nachnamen an">
         </div>
         <!-- TODO: Clientseitige Validierung: email -->
         <div class="form-group">
           <label for="email">Email *</label>
-          <input type="email" name="email" class="form-control" id="email"
+          <input type="email" name="email" maxlength="100" class="form-control" id="email"
                   value="<?php echo $email ?>"
-                  placeholder="Geben Sie Ihre Email-Adresse an.">
+                  required placeholder="Geben Sie Ihre Email-Adresse an.">
         </div>
         <!-- TODO: Clientseitige Validierung: benutzername -->
         <div class="form-group">
           <label for="username">Benutzername *</label>
-          <input type="text" name="username" class="form-control" id="username"
+          <input type="text" name="username" maxlength="30" class="form-control" id="username"
                   value="<?php echo htmlspecialchars($username) ?>"
-                  placeholder="Gross- und Keinbuchstaben, min 6 Zeichen.">
+                  required placeholder="Gross- und Keinbuchstaben, min 6 Zeichen.">
         </div>
         <!-- TODO: Clientseitige Validierung: password -->
         <div class="form-group">
           <label for="password">Password *</label>
-          <input type="password" name="password" class="form-control" id="password"
-                  placeholder="Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute">
+          <input type="password" name="password" maxlength="255" class="form-control" id="password"
+                  required placeholder="Gross- und Kleinbuchstaben, Zahlen, Sonderzeichen, min. 8 Zeichen, keine Umlaute">
         </div>
         <button type="submit" name="button" value="submit" class="btn btn-info">Senden</button>
         <button type="reset" name="button" value="reset" class="btn btn-warning">Löschen</button>
